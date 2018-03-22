6526c6ebe3808f52c34c187eb8e76854141848e5
Ryan Carlton <ryan.lee.carlton@gmail.com>
2018-03-22 16:03:45 -0500

    Adding fonts, and sass/scss, and a real component for the render

--- frontend/components/Test1/Test1.jsx
+++ frontend/components/Test1/Test1.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './Test1.scss';
 
 class Test1 extends Component {
   render() {
     return (
-        <div>
-          <header>
+        <div className="Test1">
+          <header className="font-override">
             <h1>Test 1</h1>
           </header>
           <p>hello WORLD</p>
